dc42bfd5f42be9721f303ec22556a014f6a746ad
aalexblade <79399963+aalexblade@users.noreply.github.com>
2024-02-08 20:38:42 +0200

    render

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { fetchBreeds, fetchCatByBreed } from "./cat-api"
 import SlimSelect from 'slim-select'
 import 'slim-select/dist/slimselect.css';
 
-
 const selector = document.querySelector('select.breed-select')
 const catInfo = document.querySelector('.cat-info')
 const loader = document.querySelector('.loader')
@@ -14,12 +13,12 @@ let arrBreedsId = []
 fetchBreeds()
     .then(data => {
         data.forEach(element => {
-            arrBreedsId.push({ text: element.name, value: element.id })
+            arrBreedsId.push({ text: element.name, value: element.id });
 
         });
         new SlimSelect({
             select: selector,
-            data: arrBreedsId
+            data: arrBreedsId,
         })
     })
     .catch(error => console.error(error))
@@ -30,14 +29,17 @@ selector.addEventListener('change', onSelectorChange)
 function onSelectorChange(evt) {
     const breedId = evt.currentTarget.value
 
-    fetchCatByBreed(breedId)
-        .then(data => (catInfo.innerHTML = createMarcup(data)))
-        .catch(error => console.log(error))
+    if (breedId === 'abys') {
+        catInfo.innerHTML = ''
+    } else {
+        fetchCatByBreed(breedId)
+            .then(data => (catInfo.innerHTML = createMakcup(data)))
+            .catch(error => console.log(error))
+    }
 
 }
 
-function createMarcup(arr) {
-
+function createMakcup(arr) {
     return arr.map(({ url, breeds: { 0: { name, temperament, description, wikipedia_url } } }) =>
         `<li class="list cats_list">
     <img src="${url}" alt="${name}" weight="300px" height="300px">
@@ -48,5 +50,6 @@ function createMarcup(arr) {
    <a href="${wikipedia_url}"> Wikipedia Info </a>
   </li>`
     ).join('')
+
 };
 
