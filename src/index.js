fdcd6596269d611c510e8a5c94fa992752cb2ec1
aalexblade <79399963+aalexblade@users.noreply.github.com>
2024-02-08 20:38:11 +0200

    render

--- src/index.js
+++ src/index.js
@@ -17,7 +17,6 @@ fetchBreeds()
             arrBreedsId.push({ text: element.name, value: element.id })
 
         });
-
         new SlimSelect({
             select: selector,
             data: arrBreedsId
@@ -26,34 +25,28 @@ fetchBreeds()
     .catch(error => console.error(error))
 
 
-selector.addEventListener('change', onChange)
+selector.addEventListener('change', onSelectorChange)
 
-function onChange(evt) {
-    evt.preventDefault()
+function onSelectorChange(evt) {
     const breedId = evt.currentTarget.value
 
     fetchCatByBreed(breedId)
         .then(data => (catInfo.innerHTML = createMarcup(data)))
         .catch(error => console.log(error))
+
 }
 
 function createMarcup(arr) {
 
-    // if (!arr === arr[0]) {
-    //     return console.log(arr)
-    // }
-
     return arr.map(({ url, breeds: { 0: { name, temperament, description, wikipedia_url } } }) =>
-        `  <li class="list cats_list">
+        `<li class="list cats_list">
     <img src="${url}" alt="${name}" weight="300px" height="300px">
     <h2>${name}</h2>
     <p>${description}</p>
-    <h3>Temperament</h3> 
+    <h3>Temperament</h3>
     <p>${temperament}</p>
    <a href="${wikipedia_url}"> Wikipedia Info </a>
   </li>`
     ).join('')
-
-
 };
 
